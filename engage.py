f9f45d4ec6b537dfe4d2887f0c6e4beb6420d2a1
Eric Plaster <eric.plaster@myxer.com>
2014-06-13 08:41:21 -0500

    Better, faster... etc..

--- engage.py
+++ engage.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 
+"""Engage
+Link up all the dot files from the github backed up files.
+
+Usage:
+  engage.py [-vd]
+  engage.py (-h | --help)
+  engage.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -v --verbose  Verbose
+  -d --dry-run  Dry run (assumes verbose)
+
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Eric Plaster'
+
+from docopt import docopt
+
 import os
-import stat
-from os.path import join, exists, dirname, basename, abspath, realpath, islink
+from os.path import join, exists, dirname, basename, abspath, realpath, islink, isdir
 from glob import glob
 
 HERE = dirname(abspath(__file__))
@@ -11,83 +31,119 @@ HOME = os.environ['HOME']
 # TODO: Add script arguments: verbose, force, etc
 VERBOSE = False
 
-skipfiles = [
-    '.ropeproject',
-    '.git',
-]
 
+class Linker(object):
+    skipfiles = ['.ropeproject', '.git']
 
-def backup_file(filename, dry_run=False):
-    if not dry_run:
-        os.rename(filename, filename + '.orig')
+    def __init__(self, options):
+        self.opts = options
+        self.verbose = options['--verbose']
+        self.dry_run = options['--dry-run']
 
+    def _backup_file(self, filename):
+        """ Backup files to <filename>.orig in order to recover if it was a mistake
+        """
+        if not self.dry_run:
+            if self.verbose:
+                print "Backing up file {}".format(filename)
+            os.rename(filename, filename + '.orig')
 
-def _get_excluded_files(origin, excludes):
-    if excludes:
-        excludes = excludes + skipfiles
-    else:
-        excludes = skipfiles
-
-    exfiles = []
-    for exclude in excludes:
-        exfiles += glob(join(origin, exclude))
-    return set(exfiles)
-
-
-def symtastico(origin, destination, include, excludes=None, dry_run=False):
-    exfiles = _get_excluded_files(origin, excludes)
-    files = set(glob(join(origin, include))) - set(exfiles)
-
-    work = {}
-    for f in files:
-        slink = join(destination, basename(f))
-
-        if exists(slink):
-            if islink(slink):
-                if f == realpath(slink):
-                    if VERBOSE:
-                        print "Symbolic link is correct: {} ...skipping".format(slink)
-                else:
-                    print "[WARN] unlinking {}".format(slink)
-                    if not dry_run:
-                        os.unlink(slink)
-                    work[slink] = f
-            else:
-                backup_file(slink, dry_run=dry_run)
-                print "[WARN] Backed up {} and replacing with symlink...".format(slink)
-                work[slink] = f
+    def _get_excluded_files(self, origin, excludes):
+        """ Remove excluded files from origin directory set of files and return them
+        """
+        if excludes:
+            excludes = excludes + self.skipfiles
         else:
-            work[slink] = f
+            excludes = self.skipfiles
 
-    if not work:
-        print "No work to do..."
+        exfiles = []
+        for exclude in excludes:
+            exfiles += glob(join(origin, exclude))
+        return set(exfiles)
 
-    for slink, orig in work.items():
-        if not dry_run:
-            os.symlink(orig, slink)
-        print "Creating symbolic link {} to {}".format(slink, orig)
+    def _relink(self, slink, orig):
+        """ Remove the old link (or backup if it's a file) and link to correct file.
+        """
+        if islink(slink):
+            if orig != realpath(slink):
+                if self.verbose:
+                    print "[WARN] relinking {}".format(slink)
 
-# all the dot files first
-symtastico(HERE, HOME, '.*', excludes=["*~", ".*~", ".ssh"])
+                if not self.opts['dry_run']:
+                    os.unlink(slink)
+                    os.symlink(orig, slink)
+            else:
+                if self.verbose:
+                    print "Symbolic link is correct: {} ...skipping".format(slink)
+        else:
+            if exists(slink):
+                self._backup_file(slink)
+            if not self.dry_run:
+                os.symlink(orig, slink)
 
-# bin directory...
-if not exists(join(HOME, 'bin')):
-    os.makedirs(join(HOME, 'bin'))
-symtastico(join(HERE, 'bin'), join(HOME, 'bin'), '*', excludes=["*~", ".*~"])
+    def symtastico(self, origin, destination, include, excludes=None):
+        """ Given an origin directory and a destination directory, link files from destination to origin
+        """
+        exfiles = self._get_excluded_files(origin, excludes)
+        files = set(glob(join(origin, include))) - set(exfiles)
 
-# ssh directory...
-sshdir = join(HOME, '.ssh')
-if not exists(sshdir):
-    os.makedirs(sshdir)
+        for f in files:
+            slink = join(destination, basename(f))
 
-if os.stat(sshdir).st_mode != 16832:
-    os.chmod(sshdir, 0700)
+            if not isdir(slink):
+                self._relink(slink, f)
 
-symtastico(join(HERE, '.ssh'), sshdir, '*', excludes=["*~", ".*~"])
 
-excludes = [join(sshdir, 'config'), join(sshdir, 'known_hosts')]
-keys = set(glob(join(sshdir, '*'))) - set(excludes)
-for key in keys:
-    if os.stat(key).st_mode != 33152:
-        print "Changing permisions on key {}".format(key)
-        os.chmod(key, 0600)
+def _ssh_dir(linker):
+    """ Link up files in the .ssh directory
+    """
+    sshdir = join(HOME, '.ssh')
+    if not exists(sshdir):
+        os.makedirs(sshdir)
+
+    if os.stat(sshdir).st_mode != 16832:
+        os.chmod(sshdir, 0700)
+
+    linker.symtastico(
+        join(HERE, '.ssh'), sshdir, '*', excludes=["*~", ".*~"])
+
+    excludes = [join(sshdir, 'config'), join(sshdir, 'known_hosts')]
+    keys = set(glob(join(sshdir, '*'))) - set(excludes)
+    for key in keys:
+        if os.stat(key).st_mode != 33152:
+            print "Changing permisions on key {}".format(key)
+            os.chmod(key, 0600)
+
+
+def main(options):
+    """ Main function to rule them all
+    """
+    linker = Linker(options)
+
+    #--------------------------------------------------------------------------------
+    # all the dot files first
+    #--------------------------------------------------------------------------------
+    linker.symtastico(
+        HERE, HOME, '.*', excludes=["*~", ".*~", ".ssh"])
+
+    #--------------------------------------------------------------------------------
+    # bin directory...
+    #--------------------------------------------------------------------------------
+    if not exists(join(HOME, 'bin')):
+        os.makedirs(join(HOME, 'bin'))
+
+    linker.symtastico(
+        join(HERE, 'bin'), join(HOME, 'bin'), '*', excludes=["*~", ".*~"])
+
+    #--------------------------------------------------------------------------------
+    # ssh directory
+    #--------------------------------------------------------------------------------
+    _ssh_dir(linker)
+
+if __name__ == '__main__':
+    options = docopt(__doc__, version=__version__)
+
+    if options['--dry-run']:
+        options['--verbose'] = True
+
+    main(options)
