43a7b9ca1f6428ff496fd5cd882d1621a7d0f28b
Eric Plaster <eric.plaster@myxer.com>
2014-06-13 08:40:52 -0500

    Added beginnings of a full re-install script using homebrew cask

---
+++ brew_install.sh
@@ -0,0 +1,32 @@
+# Homebrew
+#   http://brew.sh/
+# Homebrew Cask
+#   http://caskroom.io/
+# 
+
+# Before we begin:
+#
+# * Install XCode
+# * Install XQuartz: https://xquartz.macosforge.org
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+# brew cask
+brew tap caskroom/cask
+brew install brew-cask
+
+## Lib
+brew install libevent
+brew install libxml2
+brew install libxslt
+
+
+
+
+
+
+
+
+
+
+
